55fde8e332a75ae91672656b34d0fa98ae14503d
+3 <63890223+yomik-js@users.noreply.github.com>
2023-12-29 21:51:16 +0800

    Create defaults.py

---
+++ defrcn/config/defaults.py
@@ -0,0 +1,45 @@
+from detectron2.config.defaults import _C
+from detectron2.config import CfgNode as CN
+
+_CC = _C
+
+# ----------- Backbone ----------- #
+_CC.MODEL.BACKBONE.FREEZE = False
+_CC.MODEL.BACKBONE.FREEZE_AT = 3
+
+# ------------- RPN -------------- #
+_CC.MODEL.RPN.FREEZE = False
+_CC.MODEL.RPN.ENABLE_DECOUPLE = False
+_CC.MODEL.RPN.BACKWARD_SCALE = 1.0
+
+# ------------- ROI -------------- #
+_CC.MODEL.ROI_HEADS.NAME = "CommonalityROIHeads"
+_CC.MODEL.ROI_HEADS.FREEZE_FEAT = False
+_CC.MODEL.ROI_HEADS.ENABLE_DECOUPLE = False
+_CC.MODEL.ROI_HEADS.BACKWARD_SCALE = 1.0
+_CC.MODEL.ROI_HEADS.OUTPUT_LAYER = "DoubleFastRCNNOutputLayers"
+_CC.MODEL.ROI_HEADS.CLS_DROPOUT = False
+_CC.MODEL.ROI_HEADS.DROPOUT_RATIO = 0.8
+_CC.MODEL.ROI_HEADS.alpha = 1.0
+_CC.MODEL.ROI_HEADS.beta = 1.0  
+_CC.MODEL.ROI_BOX_HEAD.POOLER_RESOLUTION = 7  # for faster
+
+
+# ------------- TEST ------------- #
+_CC.TEST.PCB_ENABLE = False
+_CC.TEST.PCB_MODELTYPE = 'resnet'             # res-like
+_CC.TEST.PCB_MODELPATH = ""
+_CC.TEST.PCB_ALPHA = 0.50
+_CC.TEST.PCB_UPPER = 1.0
+_CC.TEST.PCB_LOWER = 0.05
+
+# ------------ Other ------------- #
+_CC.SOLVER.WEIGHT_DECAY = 5e-5
+_CC.MUTE_HEADER = True
+_CC.MODEL.ROI_HEADS.MEMORY = True
+_CC.MODEL.ROI_HEADS.QUEUE_LEN = 512
+_CC.MODEL.ROI_HEADS.AUGMENTATION = False
+_CC.MODEL.ROI_HEADS.SAVE = False
+_CC.MODEL.ROI_HEADS.Shot = 5
+_CC.DATASETS.SPLIT = 2
+_CC.MODEL.ROI_HEADS.num = 200
